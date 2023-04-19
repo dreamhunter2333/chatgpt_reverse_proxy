9f1a9d194218fb25f967ddb7406cfabdec952d9c
jinmu333 <dreamhunter2333@gmail.com>
2023-04-20 00:24:35 +0800

    feature: add refersh_access_token_lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import uvicorn
+import asyncio
 
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,8 +8,10 @@ from fastapi.responses import PlainTextResponse, Response
 from playwright.async_api import async_playwright, Page
 
 from config import settings
+import json
 
 ACCESS_TOKEN = None
+refersh_access_token_lock = asyncio.Lock()
 _logger = logging.getLogger(__name__)
 
 
@@ -42,8 +45,19 @@ async def exception_handler(request: Request, exc: Exception):
 
 async def refersh_access_token(page: Page):
     global ACCESS_TOKEN
-    if settings.auto_refersh_access_token and not ACCESS_TOKEN:
+    async with refersh_access_token_lock:
+        if not settings.auto_refersh_access_token or ACCESS_TOKEN:
+            return
         await page.goto(settings.base_url)
+        try:
+            async with page.expect_response("https://chat.openai.com/api/auth/session") as session:
+                value = await session.value
+                value_json = await value.json()
+                ACCESS_TOKEN = value_json["accessToken"]
+                _logger.info("Refreshed access token")
+                return
+        except Exception as e:
+            _logger.exception("Failed to refresh access token", e)
         try:
             await page.locator('//iframe[contains(@src, "cloudflare")]').wait_for(timeout=settings.checkbox_timeout)
             handle = await page.query_selector('//iframe[contains(@src, "cloudflare")]')
@@ -57,10 +71,15 @@ async def refersh_access_token(page: Page):
             )
         except Exception as e:
             _logger.exception("Checkbox not found", e)
-        async with page.expect_response("https://chat.openai.com/api/auth/session") as session:
-            value = await session.value
-            value_json = await value.json()
-            ACCESS_TOKEN = value_json["accessToken"]
+        try:
+            async with page.expect_response("https://chat.openai.com/api/auth/session") as session:
+                value = await session.value
+                value_json = await value.json()
+                ACCESS_TOKEN = value_json["accessToken"]
+                _logger.info("Refreshed access token")
+                return
+        except Exception as e:
+            _logger.exception("Failed to refresh access token", e)
 
 
 @app.get("/admin/refersh_access_token")
@@ -98,7 +117,7 @@ async def _reverse_proxy(request: Request):
         body = (
             "null"
             if request.method.upper() in ("GET", "DELETE")
-            else f"'{body.decode()}'"
+            else f"{json.dumps(body.decode(), ensure_ascii=False)}"
         )
         target_path = f"{request.url.path}?{request.url.query}" if request.url.query else request.url.path
         result = await page.evaluate('''
